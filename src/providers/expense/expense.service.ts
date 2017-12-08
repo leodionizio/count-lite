297a393d941f787222ecc58789ec0d81e8e55a4c
Leonardo Dionizio <leodionizio13@gmail.com>
2017-12-08 16:59:27 -0200

    Atualização do serviços para correção de bugs de script

--- src/providers/expense/expense.service.ts
+++ src/providers/expense/expense.service.ts
@@ -10,7 +10,7 @@ export class ExpenseService {
 	private db: SQLiteObject;
 	private isFirstCall: boolean = true;
 
-	constructor(public sqliteConnService: SqliteConnService) {}
+	constructor(public sqliteConnService: SqliteConnService) { }
 
 	private getDb(): Promise<SQLiteObject> {
 		if (this.isFirstCall) {
@@ -20,21 +20,21 @@ export class ExpenseService {
 				this.db = db;
 				this.db
 					.executeSql(
-						`
-          CREATE TABLE IF NOT EXISTS expenses 
-          (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            month INTEGER
-            description TEXT
-            value REAL
-          )`,
-						{}
+					`
+						CREATE TABLE IF NOT EXISTS expenses 
+						(
+							id INTEGER PRIMARY KEY AUTOINCREMENT,
+							month INTEGER,
+							description TEXT,
+							value REAL
+						)`,
+					{}
 					)
 					.then(success => {
-						console.log('Tabela criada com sucesso', success);
+						console.log('Tabela criada gasto com sucesso', success);
 					})
 					.catch(error => {
-						console.log('erro ao criar tabela', error);
+						console.log('Erro ao criar tabela gasto', error);
 					});
 				return this.db;
 			});
@@ -46,11 +46,11 @@ export class ExpenseService {
 		return this.getDb().then((db: SQLiteObject) => {
 			return <Promise<ExpenseModel[]>>this.db
 				.executeSql(
-					`
-          SELECT id, month, decription, value
-          FROM expenses
-          ORDER BY id ${orderBy || 'DESC'}`,
-					{}
+				`
+					SELECT id, month, description, value
+					FROM expenses
+					ORDER BY id ${orderBy || 'DESC'}`,
+				{}
 				)
 				.then(resultSet => {
 					let list: ExpenseModel[] = [];
@@ -64,63 +64,75 @@ export class ExpenseService {
 	}
 
 	public create(expense: ExpenseModel): Promise<ExpenseModel> {
-		return this.db
-			.executeSql(
-				`
-        INSERT INTO expense (month, description, value)
-        VALUES (?, ?, ?)`,
-				[expense.month, expense.description, expense.value]
-			)
-			.then(resultSet => {
-				expense.id = resultSet.insertId;
-				return expense;
+		return this.getDb()
+			.then((db: SQLiteObject) => {
+				return this.db
+					.executeSql(
+					`
+						INSERT INTO expenses (month, description, value)
+						VALUES (?, ?, ?)`,
+					[expense.month, expense.description, expense.value]
+					)
+					.then(resultSet => {
+						expense.id = resultSet.insertId;
+						return expense;
+					})
+					.catch((error: Error) => {
+						console.log(`Erro ao criar '${expense.description}' conta!`, error);
+						return expense;
+					});
 			})
-			.catch((error: Error) => {
-				console.log(`Erro ao criar '${expense.description}' conta!`, error);
-				return expense;
-			});
 	}
 
 	public update(expense: ExpenseModel): Promise<boolean> {
-		return this.db
-			.executeSql(
-				`
-        UPDATE expense
-        SET month = ?, description = ?, value = ?
-        WHERE id= ?`,
-				[expense.month, expense.description, expense.value, expense.id]
-			)
-			.then(resultSet => resultSet.rowsAffected >= 0)
-			.catch((error: Error) => {
-				console.log(`Erro ao atualizar conta!`, error);
-				return false;
-			});
+		return this.getDb()
+			.then((db: SQLiteObject) => {
+				return this.db
+					.executeSql(
+					`
+						UPDATE expenses
+						SET month = ?, description = ?, value = ?
+						WHERE id= ?`,
+					[expense.month, expense.description, expense.value, expense.id]
+					)
+					.then(resultSet => resultSet.rowsAffected >= 0)
+					.catch((error: Error) => {
+						console.log(`Erro ao atualizar conta!`, error);
+						return false;
+					});
+			})
 	}
 
 	public delete(id: number): Promise<boolean> {
-		return this.db
-			.executeSql('DELETE FROM expense WHERE id=?', [id])
-			.then(resultSet => resultSet.rowsAffected >= 0)
-			.catch((error: Error) => {
-				console.log(`Erro ao deletar conta!`, error);
-				return false;
-			});
+		return this.getDb()
+			.then((db: SQLiteObject) => {
+				return this.db
+					.executeSql('DELETE FROM expenses WHERE id=?', [id])
+					.then(resultSet => resultSet.rowsAffected >= 0)
+					.catch((error: Error) => {
+						console.log(`Erro ao deletar conta!`, error);
+						return false;
+					});
+			})
 	}
 
 	public getById(id: number): Promise<ExpenseModel> {
-		return this.db
-			.executeSql(
-				`
-        SELECT id, month, decription, value
-        FROM expenses
-        WHERE id=?`,
-				[id]
-			)
-			.then(resultSet => {
-				return resultSet.rows.item(0);
+		return this.getDb()
+			.then((db: SQLiteObject) => {
+				return this.db
+					.executeSql(
+					`
+						SELECT id, month, decription, value
+						FROM expenses
+						WHERE id=?`,
+					[id]
+					)
+					.then(resultSet => {
+						return resultSet.rows.item(0);
+					})
+					.catch((error: Error) => {
+						console.log(`Erro ao encontrar conta!`, error);
+					});
 			})
-			.catch((error: Error) => {
-				console.log(`Erro ao encontrar conta!`, error);
-			});
 	}
 }
--- src/providers/income/income.service.ts
+++ src/providers/income/income.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { SQLiteObject } from '@ionic-native/sqlite';
 import { SqliteConnService } from '../sqlite-conn/sqlite-conn.service';
+import { IncomeModel } from '../../models/income.model';
 
 @Injectable()
 export class IncomeService {
@@ -10,33 +11,127 @@ export class IncomeService {
 
   constructor(
     public sqliteConnService: SqliteConnService
-  ) {}
+  ) { }
 
   private getDb(): Promise<SQLiteObject> {
-  if (this.isFirstCall) {
-    this.isFirstCall = false;
+    if (this.isFirstCall) {
+      this.isFirstCall = false;
 
-    return this.sqliteConnService.getDb('count.db')
-      .then((db: SQLiteObject) => {
-        this.db = db;
-        this.db.executeSql(`CREATE TABLE IF NOT EXISTS income
+      return this.sqliteConnService.getDb('count.db')
+        .then((db: SQLiteObject) => {
+          this.db = db;
+          this.db.executeSql(`CREATE TABLE IF NOT EXISTS incomes
           (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            value REAL,
             month INTEGER,
             description TEXT,
+            value REAL
           )`, {})
-          .then((success) => {
-            console.log('Tabela renda criada com sucesso', success);
+            .then((success) => {
+              console.log('Tabela renda criada com sucesso', success);
+            })
+            .catch((error) => {
+              console.log('Erro ao criar tabela renda', error);
+            })
+          return this.db;
+        })
+    }
+    return this.sqliteConnService.getDb();
+  }
+
+  public getAll(orderBy?: string): Promise<IncomeModel[]> {
+    return this.getDb().then((db: SQLiteObject) => {
+      return <Promise<IncomeModel[]>>this.db
+        .executeSql(
+        `
+        SELECT id, month, description, value
+        FROM incomes
+        ORDER BY id ${orderBy || 'DESC'}`,
+        {}
+        )
+        .then(resultSet => {
+          let list: IncomeModel[] = [];
+          for (let i = 0; i < resultSet.rows.length; i++) {
+            list.push(resultSet.rows.item(i));
+          }
+          return list;
+        })
+        .catch((error: Error) => console.log('Erro', error));
+    });
+  }
+
+  public create(income: IncomeModel): Promise<IncomeModel> {
+    return this.getDb()
+      .then((db: SQLiteObject) => {
+        return this.db
+          .executeSql(
+          `
+            INSERT INTO incomes (month, description, value)
+            VALUES (?, ?, ?)`,
+          [income.month, income.description, income.value]
+          )
+          .then(resultSet => {
+            income.id = resultSet.insertId;
+            return income;
           })
-          .catch((error) => {
-            console.log('Erro ao criar tabela renda', error);
-          })
-        return this.db;
+          .catch((error: Error) => {
+            console.log(`Erro ao criar '${income.description}' renda!`, error);
+            return income;
+          });
       })
   }
-  return this.sqliteConnService.getDb();
-}
+
+  public update(income: IncomeModel): Promise<boolean> {
+    return this.getDb()
+      .then((db: SQLiteObject) => {
+        return this.db
+          .executeSql(
+          `
+            UPDATE incomes
+            SET month = ?, description = ?, value = ?
+            WHERE id= ?`,
+          [income.month, income.description, income.value, income.id]
+          )
+          .then(resultSet => resultSet.rowsAffected >= 0)
+          .catch((error: Error) => {
+            console.log(`Erro ao atualizar renda!`, error);
+            return false;
+          });
+      })
+  }
+
+  public delete(id: number): Promise<boolean> {
+    return this.getDb()
+      .then((db: SQLiteObject) => {
+        return this.db
+          .executeSql('DELETE FROM incomes WHERE id=?', [id])
+          .then(resultSet => resultSet.rowsAffected >= 0)
+          .catch((error: Error) => {
+            console.log(`Erro ao deletar renda!`, error);
+            return false;
+          });
+      })
+  }
+
+  public getById(id: number): Promise<IncomeModel> {
+    return this.getDb()
+      .then((db: SQLiteObject) => {
+        return this.db
+          .executeSql(
+          `
+            SELECT id, month, decription, value
+            FROM incomes
+            WHERE id=?`,
+          [id]
+          )
+          .then(resultSet => {
+            return resultSet.rows.item(0);
+          })
+          .catch((error: Error) => {
+            console.log(`Erro ao encontrar renda!`, error);
+          });
+      })
+  }
 
 
 }
